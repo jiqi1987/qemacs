85042327efb9cdc4eadc2b27800401d06d1ce1be
Qi Ji <jiqi1987@yahoo.com>
2022-05-29 20:58:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+sudo apt update
+sudo apt -y install emacs-nox
+sudo apt -y install net-tools binutils g++ cmake
+
+cd file/lisp/
+git clone https://github.com/dominikh/go-mode.el.git
+cd ../..
+
 rm -rf ~/.emacs.d
 cp -r file ~/.emacs.d
 cp init ~/.emacs
