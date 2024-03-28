2be623df3b08850d850844dcb5ae96d8d0571f4f
Qi Ji <jiqi1987@yahoo.com>
2024-03-28 15:05:34 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 sudo apt update
 sudo apt -y install emacs-nox
 sudo apt -y install net-tools binutils g++ cmake
+sudo apt -y install bash-completion
+sudo apt -y install git
+
+source ./git-completion.bash
 
 cd file/lisp/
 git clone https://github.com/dominikh/go-mode.el.git
